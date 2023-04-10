b1a3964545e74d3c581a9993365ee04100e98bad
amiriar <abdolrahimiamirreza@gmail.com>
2023-04-10 20:43:53 +0430

    again and again..

--- app.react/src/app.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class APp extends React.Component{
-    render() {
-        return <h1>amiriar</h1>
-    }
-}
-export default APp;
--- app.react/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-
-import APp from "./app";
-ReactDOM.render(<APp />, document.getElementById("root"))
