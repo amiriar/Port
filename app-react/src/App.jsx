c16e328eb926c6e63fcd9ecb931815e2bb5c4539
amiriar <abdolrahimiamirreza@gmail.com>
2023-04-11 21:06:38 +0430

    hichi ke hichi

--- app-react/src/App.jsx
+++ app-react/src/App.jsx
@@ -8,19 +8,19 @@ function App() {
     <div className="App">
       <header>
         <nav>
-          <h2 className='name'>AMIRIAR</h2>
+        <a href="#1"><h2 className='name'>AMIRIAR</h2></a>
           <ul>
             <li>Home</li>
             <li>Works</li>
             <li>About</li>
           </ul>
         </nav>
-        <article>
+        <article id='1'>
           <div className="id">
             <div className='left'>
-              <h1>Hi, My name is Amirreza Abdolrahimi and i'm a frontend web devloper</h1>
+              <h1 className='tozih'>Hi, My name is Amirreza Abdolrahimi and i'm a frontend web devloper!</h1>
             </div>
-            <div className='right'></div>
+            <div className='right'><img className='meimg' src="/icon.png" alt="amir's photo"/></div>
           </div>
         </article>
       </header>
