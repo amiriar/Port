ffe5296580d68a0b465193e4ad8cd55a83583c5d
amiriar <abdolrahimiamirreza@gmail.com>
2023-04-11 18:07:46 +0430

    finnaly a new start

---
+++ app-react/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <h2>amiriar</h2>
+          <ul>
+            <li>Home</li>
+            <li>Works</li>
+            <li>About</li>
+          </ul>
+        </nav>
+        <article></article>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
--- react/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react";
-
-ReactDOM.render(<h1>Salam!</h1>, document.getElementById("root"));
-
