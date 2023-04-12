91f83f6f8d1ca55889a224e80c5d299f7f816262
amiriar <abdolrahimiamirreza@gmail.com>
2023-04-12 11:01:41 +0430

    uh fuck

--- app-react/src/App.jsx
+++ app-react/src/App.jsx
@@ -18,7 +18,14 @@ function App() {
         <article id='1'>
           <div className="id">
             <div className='left'>
-              <h1 className='tozih'>Hi, My name is Amirreza Abdolrahimi and i'm a frontend web devloper!</h1>
+              <h2 className='tozih'>Hi, My name is Amirreza Abdolrahimi and i'm a frontend web devloper! <br /> i know some of them languages like:</h2>
+              <div className='langs'>
+                <img src="/html.png" alt="html" className='langpic' draggable="false"/>
+                <img src="/css.png" alt="css" className='langpic' draggable="false"/>
+                <img src="/sass.png" alt="sass" className='langpic' draggable="false"/>
+                <img src="js.png" alt="js" className='langpic' draggable="false"/>
+                <img src="react-img.png" alt="react" className='langpic' draggable="false" />
+              </div>
             </div>
             <div className='right'><img className='meimg' src="/icon.png" alt="amir's photo"/></div>
           </div>
